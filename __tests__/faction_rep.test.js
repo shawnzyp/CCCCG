ff0ae37c38f17cc32a892ea00c3c0323cb652ae5
shawnzyp <shawnpeiris22@gmail.com>
2025-09-03 16:38:00 -0400

    Add faction reputation module and tests

---
+++ __tests__/faction_rep.test.js
@@ -0,0 +1,26 @@
+import { jest } from '@jest/globals';
+import { setupFactionRepTracker } from '../scripts/faction.js';
+
+describe('faction reputation tracker', () => {
+  test('gain button updates value and pushes history', () => {
+    document.body.innerHTML = `
+      <div>
+        <progress id="omni-rep-bar" max="100" value="0"></progress>
+        <span id="omni-rep-tier"></span>
+        <p id="omni-rep-perk"></p>
+        <button id="omni-rep-gain"></button>
+        <button id="omni-rep-lose"></button>
+        <input type="hidden" id="omni-rep" value="200" />
+      </div>
+    `;
+    const pushHistory = jest.fn();
+    const handlePerkEffects = jest.fn();
+
+    setupFactionRepTracker(handlePerkEffects, pushHistory);
+
+    document.getElementById('omni-rep-gain').click();
+
+    expect(document.getElementById('omni-rep').value).toBe('205');
+    expect(pushHistory).toHaveBeenCalled();
+  });
+});
