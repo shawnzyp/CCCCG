91dd821b203b87e6014d71fc12538fdd8cafea7b
shawnzyp <shawnpeiris22@gmail.com>
2025-09-30 03:27:56 -0400

    Bundle client via new entry and add catalog build pipeline

---
+++ src/client-entry.js
@@ -0,0 +1,6 @@
+import './launch-media.js';
+import './header-title.jsx';
+import './animated-titles.jsx';
+import '../scripts/main.js';
+import '../scripts/dm.js';
+import '../scripts/somf-firebase.js';
