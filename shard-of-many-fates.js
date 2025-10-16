a99acb7ade38e97b13c8a6ff25594c16a6a78800
shawnzyp <shawnpeiris22@gmail.com>
2025-10-16 01:43:29 -0400

    Fix Chronicle shard art asset reference

--- shard-of-many-fates.js
+++ shard-of-many-fates.js
@@ -201,7 +201,7 @@
     KEY: 'The Key.png',
     THRONE: 'The Throne.png',
     CRASH: 'The Crash.png',
-    CHRONICLE: 'The Chronicile.png',
+    CHRONICLE: 'The Chronicle.png',
     SUNSHARD: 'The Sunshard.png',
     MOONSHARD: 'The Moonshard.png',
     STARSHARD: 'The Starshard.png',
