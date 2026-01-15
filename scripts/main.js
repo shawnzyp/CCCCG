ed7f735c4ccd2373fd8ddb4ea74c55ea11e1bfa9
shawnzyp <shawnpeiris22@gmail.com>
2026-01-15 13:42:32 -0500

    Add login entry to main menu

--- scripts/main.js
+++ scripts/main.js
@@ -13861,7 +13861,13 @@ registerBootTask(() => {
   }
 });
 
+const btnLogin = $('btn-login');
 const btnLog = $('btn-log');
+registerBootTask(() => {
+  if (btnLogin) {
+    btnLogin.addEventListener('click', () => openLoginModal());
+  }
+});
 registerBootTask(() => {
   if (btnLog) {
     btnLog.addEventListener('click', ()=>{ renderLogs(); openNarrativeModal('modal-log'); });
